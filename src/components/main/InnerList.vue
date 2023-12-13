26cf78b354102aae1ab691f85a658f7ec8bd2c1e
danielecarpentiero <carpentierodaniele@gmail.com>
2023-12-13 11:06:22 +0100

    feat: props to component

--- src/components/main/InnerList.vue
+++ src/components/main/InnerList.vue
@@ -1,16 +1,3 @@
-<template>
-  <div class="inner-list">
-    <ul>
-      <li
-        v-for="card in store.cards"
-        :img="card.card_images[0].image_url"
-        :title="card.name"
-        :archetype="card.archetype"
-      ></li>
-    </ul>
-  </div>
-</template>
-
 <script>
 import SingleCard from "./SingleCard.vue";
 import { store } from "../../store";
@@ -26,6 +13,20 @@ export default {
   },
 };
 </script>
+
+<template>
+  <div class="inner-list">
+    <ul>
+      <li v-for="card in store.cards">
+        <SingleCard
+          :img="card.card_images[0].image_url"
+          :title="card.name"
+          :archetype="card.archetype"
+        />
+      </li>
+    </ul>
+  </div>
+</template>
 
 <style scoped>
 .inner-list {
